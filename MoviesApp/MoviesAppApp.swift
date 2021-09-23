7f29546b09cf52f70f96ec7413779d9ec6be636f
Furkan Saffet OLKAY <olkayfurkan@gmail.com>
2021-09-23 22:51:00 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Furkan Saffet Olkay on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
